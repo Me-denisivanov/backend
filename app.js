3ec0f592d8151309ba79503e54aa80de84c16d78
Денис Иванов <denisweb@MacBook-Air-Denis.local>
2022-08-25 17:07:15 +0300

    fix back

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ const routes = require('./routes');
 
 const app = express();
 
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/uploads', express.static('uploads'));
 app.use(cors());
 
 app.use('/api', routes);
+
+// const PORT = config.get('port') ?? 6050;
 
 if (process.env.NODE_ENV === 'production') {
 	app.use('/', express.static(path.join(__dirname, 'client')));
@@ -25,21 +28,19 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
-mongoose
-	.connect(process.env.MONGODB_URI)
-	.then(() => console.log('OK'))
-	.catch((err) => console.log(err));
-
 async function start() {
 	try {
 		mongoose.connection.once('open', () => {
 			initDatabase();
 		});
 
-		// console.log('MongoDB success');
+		// await mongoose.connect(config.get('mongoUri'));
+		await mongoose.connect(process.env.MONGODB_URI);
+
+		console.log('MongoDB success');
 
 		app.listen(process.env.PORT || 6050, () => {
-			console.log(`Server started on port ${process.env.PORT}...`);
+			console.log(`Server started on port ${6050}...`);
 		});
 	} catch (e) {
 		console.log(e);
