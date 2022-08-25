8851f3442df6adb9a1b18dd708af3aeca3590eb5
Денис Иванов <denisweb@MacBook-Air-Denis.local>
2022-08-25 19:03:29 +0300

    fix backdd

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ const routes = require('./routes');
 
 const app = express();
 
-//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/uploads', express.static('uploads'));
+app.use('uploads', express.static('uploads'));
 app.use(cors());
 
 app.use('/api', routes);
@@ -33,7 +32,6 @@ async function start() {
 			initDatabase();
 		});
 
-		// await mongoose.connect(config.get('mongoUri'));
 		await mongoose.connect(process.env.MONGODB_URI);
 
 		console.log('MongoDB success');
